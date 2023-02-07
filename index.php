a0b8d2fd85120f0d2c21543a7901eb5777dc7d29
Matias Alarcon <matiasignacio199408@gmail.com>
2023-02-07 16:15:22 -0300

    proyecto yuugen

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include('./template/cabecera.php'); ?>
+
+<section class="banner" id="index">
+    <div class="texto-banner">
+        <h1 data-text="Digitaliza tus proyectos." class="title-banner">Digitaliza tus proyectos.</h1>
+        <p class="p-banner"> 
+            Comunicación permanente con nuestros clientes durante todo el proceso de creación.<br><br>
+            Rapidez, eficiencia y calidad. Revisa más de nuestro trabajo <a class="link-trabajo" href="#">AQUÍ</a><br><br>
+            ¿Qué esperas? ¡Contrátanos ya y obtén un año completo de matención gratis!
+        </p>
+        <div class="btn-container">
+            <div class="btn">
+                <a href="#">¡Contrata ya!</a>
+            </div>
+        </div>
+    </div>
+    <div class="img-banner">
+        
+    </div>
+</section>
+
+<?php include('./template/pie.php'); ?>
---
+++ template/cabecera.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yuugen</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <header class="cabecera">
+        <div class="logo-menu">
+            <div class="logo-container">
+                <img src="./img/logo1.png" alt="logo" class="logo">
+            </div>
+            <ul class="nav">
+                <li><a href="#index">Inicio</a></li>
+                <li><a href="">Servicios</a></li>
+                <li><a href="">Nosotros</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </div>
+    </header>
+    
